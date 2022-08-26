2e883a374de8058fca53b57044a3a7cb4665f1b2
Jugurtha.Silem <jugurtha@Mac-mini-de-Jugurtha.local>
2022-08-26 16:52:04 +0200

    React-CountriesV1

---
+++ src/Pages/About.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Logo from '../Components/Logo';
+import Navigation from '../Components/Navigation';
+
+
+const About = () => {
+    return (
+        
+        <div>
+            <Logo/>
+            <Navigation/>
+            <h1>About</h1> 
+            
+            <br />
+
+            <p> Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consectetur officia iure alias quisquam, odio tempora eos autem. Repellendus rerum eius dolore delectus. Nostrum fuga, pariatur provident illo vitae nemo maiores!00</p> 
+            <br />
+            <br />
+
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit deserunt sint consequatur vero ea ut aliquid asperiores excepturi sapiente! Qui praesentium inventore assumenda blanditiis repudiandae id fugiat asperiores ut illo.
+            Nemo velit quisquam itaque asperiores enim aliquid. Delectus exercitationem, aut, blanditiis itaque, quasi dolores deleniti eveniet minus pariatur ullam quae reprehenderit. Dolorum quos velit, blanditiis sit in reprehenderit dolor molestiae!
+            Nostrum odit corrupti veritatis eos ipsa, autem voluptatem. Animi aspernatur voluptatibus sed eos nemo! Esse, dolorum vitae neque, ut molestiae sapiente hic impedit quos dolore, voluptates quo modi veritatis consequuntur?
+            Vitae quo illum consectetur fuga obcaecati nemo dolorum a minima? Dignissimos necessitatibus voluptas eos corrupti molestias, minus voluptates iure? Fuga, porro molestiae. Nam dolores consectetur quam explicabo fuga possimus placeat.
+            Aliquid quas eveniet voluptas vel, optio qui, ratione beatae nulla molestiae repellendus voluptatem pariatur illo, adipisci quae nostrum itaque eius cum laudantium hic? Aspernatur aliquam vel totam magnam harum esse.
+            Expedita voluptatem hic voluptate doloribus ea impedit dolore atque? Veniam perspiciatis nulla harum error, sed voluptatibus cum architecto laudantium quaerat autem tempore recusandae, iste modi doloribus dolorem. Eius, architecto error.
+            Reiciendis quas impedit repudiandae nobis maxime, modi sint maiores, ducimus iure, sit incidunt fuga hic. Perferendis illum quam facilis? Repellat perspiciatis quas similique laudantium incidunt, optio accusantium et quam ullam.
+            Quod distinctio temporibus commodi placeat adipisci natus, aut doloribus laudantium corrupti dolorem. Optio reiciendis praesentium aperiam nesciunt amet adipisci hic, magni autem perferendis quae laborum cumque, voluptas temporibus natus eveniet.
+            Magni quas veniam nostrum. Ducimus nihil tempora, doloribus amet vero vitae soluta nisi debitis qui a quisquam sint, facere fugiat odio ullam veniam quibusdam eum. Minus sed tenetur vitae quaerat?
+            Placeat facilis aspernatur quam dolorem delectus ducimus facere eaque tempora recusandae, voluptatibus voluptas consectetur quidem est sit, temporibus doloribus magni enim laboriosam magnam! Dicta non, voluptas ipsum est rerum voluptatem?0</p>
+        </div>
+    );
+};
+
+export default About;
